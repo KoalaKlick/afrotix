c2110e3f01fe9ab08c789b4131a2da538c38d066
EveryJE <kgyan19lf@outlook.com>
2026-03-19 16:38:15 +0000

    fix(onboarding): add debug output for public events in LandingPage component

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -12,6 +12,7 @@ export default async function LandingPage() {
 
   return (
     <>
+    <pre>{JSON.stringify(events, null, 2)}</pre>
       <HeroSection />
       <PanAfricanDivider />
       <FeaturesSection />
