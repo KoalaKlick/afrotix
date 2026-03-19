5728e7a84f2133e6c2cdbb8d5bb14608d929c835
EveryJE <kgyan19lf@outlook.com>
2026-03-19 18:49:43 +0000

    fix(Input): remove red background color for improved input styling

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -28,7 +28,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
                     <input
                         type={inputType}
                         className={cn(
-                            'flex h-10 w-full rounded-md border border-input bg-red-500 placeholder:opacity-70 pl-10 py-3 text-sm shadow-none transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+                            'flex h-10 w-full rounded-md border border-input placeholder:opacity-70 pl-10 py-3 text-sm shadow-none transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
                             isPassword ? 'pr-10' : 'pr-3',
                             className
                         )}
