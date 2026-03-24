4fd8bf91fb8d07b7b893d5f6279d47b6970d7b44
EveryJE <kgyan19lf@outlook.com>
2026-03-24 22:00:02 +0000

    feat: comment out debug output for public events in LandingPage

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -12,7 +12,7 @@ export default async function LandingPage() {
 
   return (
     <>
-    <pre>{JSON.stringify(events, null, 2)}</pre>
+    {/* <pre>{JSON.stringify(events, null, 2)}</pre> */}
       <HeroSection />
       <PanAfricanDivider />
       <FeaturesSection />
