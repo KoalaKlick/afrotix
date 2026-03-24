6ed0c663447a19aede6d521ba5bdc91ae21799bf
EveryJE <kgyan19lf@outlook.com>
2026-03-24 21:46:03 +0000

    Refactor code structure for improved readability and maintainability

--- components/Landing/shared/FeatureCard.tsx
+++ components/Landing/shared/FeatureCard.tsx
@@ -18,9 +18,9 @@ export function FeatureCard({
     className
 }: FeatureCardProps) {
     return (
-        <Card  variant='afro-4' className={cn( "shadow-sm backdrop-blur-md group gap-2 bg-amber-50/10 border-0 hover:shadow-sm    transition-shadow", className)}>
+        <Card  variant='afro-4' className={cn( "shadow-sm pt-8 relative backdrop-blur-md group gap-2 bg-amber-50/10 border-0 hover:shadow-sm    transition-shadow", className)}>
             <CardHeader className=''>
-                <div className=""><Icon className={cn("size-12  group-hover:scale-105 transition-all rounded-full text-white p-2", iconClassName)} /></div>
+                <div className=""><Icon className={cn("size-10  absolute -top-1 -left-1 group-hover:scale-105 transition-all rounded-tl-lg border-r-4 border-b-4 border-[#F8F7F1] text-white p-2", iconClassName)} /></div>
                 
                 <CardTitle>{title}</CardTitle>
             </CardHeader>
